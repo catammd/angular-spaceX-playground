46b6ad5a84ea44bc73ee7c6ff0087ee2faf420d0
Catalin Stefan Tatu <catalin.tatu@creuna.dk>
2020-11-21 18:38:25 +0100

    Refactoring + presentation component

---
+++ src/app/spaceX-launch-finder/components/mission-details/mission-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mission-details',
+  template: `<p>Mission details component</p> `,
+})
+export class MissionDetailsComponent {
+  constructor() {}
+}
--- src/app/spaceX-launch-finder/spaceX-launch-finder.module.ts
+++ src/app/spaceX-launch-finder/spaceX-launch-finder.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//Containers
 import { SpaceXLaunchFinderComponent } from './containers/spaceX-launch-finder/spaceX-launch-finder-component';
-
+//Components
+import { MissionDetailsComponent } from './components/mission-details/mission-details.component';
 //Pass in the config object
 @NgModule({
   //it will hold all of the components related to this module
-  declarations: [SpaceXLaunchFinderComponent],
+  declarations: [SpaceXLaunchFinderComponent, MissionDetailsComponent],
   imports: [CommonModule],
   exports: [SpaceXLaunchFinderComponent],
 })
