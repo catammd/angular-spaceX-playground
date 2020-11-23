72286cfcbc478ba0989604a28758ce82875f3ca4
Catalin Stefan Tatu <catalin.tatu@creuna.dk>
2020-11-23 20:48:00 +0100

    More fine-tuning

--- src/app/spaceX-launch-finder/containers/spaceX-launch-finder/spaceX-launch-finder-component.ts
+++ src/app/spaceX-launch-finder/containers/spaceX-launch-finder/spaceX-launch-finder-component.ts
@@ -9,15 +9,15 @@ import { LaunchInterface } from '../../models/launch.interface';
     <h1>Latest Space X Launch</h1>
     <div class="c-latest-mission">
       <mission-details [mission]="latestLaunch"></mission-details>
-      <h3>All Space X Launches</h3>
-      <div class="c-text-input">
-        <input
-          class="form-control"
-          type="text"
-          placeholder="Search for a launch"
-          [(ngModel)]="searchText"
-        />
-      </div>
+    </div>
+    <h3>All Space X Launches</h3>
+    <div class="c-text-input">
+      <input
+        class="form-control"
+        type="text"
+        placeholder="Search for a launch"
+        [(ngModel)]="searchText"
+      />
     </div>
     <div class="c-card-container">
       <mission-details
