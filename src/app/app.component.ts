fa1fabe3e4859ba366353869cbbd0a520f0ef78c
Catalin Stefan Tatu <catalin.tatu@creuna.dk>
2020-11-23 19:54:25 +0100

    Fine-tunning UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
   template: `
     <div class="content" role="main">
       <spacex-launch-finder></spacex-launch-finder>
--- src/app/spaceX-launch-finder/containers/spaceX-launch-finder/spaceX-launch-finder-component.ts
+++ src/app/spaceX-launch-finder/containers/spaceX-launch-finder/spaceX-launch-finder-component.ts
@@ -7,15 +7,18 @@ import { LaunchInterface } from '../../models/launch.interface';
   styleUrls: ['spaceX-launch-finder.component.scss'],
   template: `
     <h1>Latest Space X Launch</h1>
-    <mission-details [mission]="latestLaunch"></mission-details>
-    <h3>All Space X Launches</h3>
-
-    <input
-      class="form-control"
-      type="text"
-      [(ngModel)]="searchText"
-      placeholder="Search"
-    />
+    <div class="c-latest-mission">
+      <mission-details [mission]="latestLaunch"></mission-details>
+      <h3>All Space X Launches</h3>
+      <div class="c-text-input">
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Search for a launch"
+          [(ngModel)]="searchText"
+        />
+      </div>
+    </div>
     <div class="c-card-container">
       <mission-details
         class="c-card"
